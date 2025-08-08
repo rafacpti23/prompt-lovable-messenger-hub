6812c7700b8d3ed0563dbbb6f129c3b98f0f1b1b
Rafa martins <rafacpti@gmail.com>
2025-08-08 01:56:49 -0300

    [dyad] wrote 1 file(s)

--- supabase/migrations/20250809000000-install-pgmq.sql
+++ supabase/migrations/20250809000000-install-pgmq.sql
@@ -2,5 +2,5 @@
 CREATE EXTENSION IF NOT EXISTS pgmq;
 
 -- Cria a fila 'whatsapp_campaigns' se ela ainda não existir
--- Esta é a fila que usaremos para as mensagens das campanhas
-SELECT pgmq_create('whatsapp_campaigns');
+-- A função correta é pgmq.create_queue(text)
+SELECT pgmq.create_queue('whatsapp_campaigns');
